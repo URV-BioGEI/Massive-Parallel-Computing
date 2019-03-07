05bd4f95150354c795b0f31dca54e8aebbcd9810
Aleix Mariné Tena <aleix.marine@estudiants.urv.cat>
2019-03-07 15:04:19 +0100

    added versions of the same code

--- src/QS_Secuencial.c
+++ src/QS_Secuencial.c
@@ -1,124 +1,98 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <omp.h>
+#include <assert.h>  
 
-#define NN 384000000  // 384000000 int * (4 B/ 1 int) * (1 GB / 2^30 B) = 1,43 GB de dades (com a màxim) carregades a memoria
-#define MAX_INT ((int) ((unsigned int) (-1) >> 1) )  // Definim el valor màxim d'un enter segons la màquina
+#define NN 384000000
+#define MAX_INT ((int)((unsigned int)(-1)>>1))
 
-int valors[NN + 1];  
-int valors2[NN + 1];
+int valors[NN+1];
+int valors2[NN+1];
 
-// Funció quicksort
-// int pointer *val: Punter que apunta a un vector de dades compartit en memoria. 
-// int ne: Indica la quantitat de dades que podem modificar del nostre vector apuntat per *val.
-// per tant l'índex màxim per a accedir al vector *val és ne - 1 
 void qs(int *val, int ne)
-{
-	int i, f;  // i es l'index per l'esquerra, f index per la dreta 
-	int pivot, vtmp, vfi;  // valor pivot contra el que ordenarem, vtmp valor que estem ordenant, vfi variable auxiliar que conté el valor a ordenar
-	//quan acabem de colocar un valor DESPRÉS del pivot, ja que no tenim espais buits i la necessitem.
+ {
+  int i,f,j;
+  int pivot,vtmp,vfi;
 
-	pivot = val[0];  // Inicialment escollim arbitrariament el valor de la primera posicio com a pivot
-	i = 1;  // índex mínim per a accedir al vector (comencem a i = 1 perquè a i = 0 ja hi tenim al pivot)
-	f = ne - 1;  // índex màxim per a accedir al nostre vector global
-	vtmp = val[i];  // emmagatzemmem de manera temporal el valor a la posició val[1] per a tenir el primer valor a col·locar
+  pivot = val[0];
+  i = 1;
+  f = ne-1; 
+  vtmp = val[i];
 
-	while (i <= f)  // mentre el índex inicial no superi a l'índex final
-	{
-		if (vtmp < pivot)  // Si el valor temporal (el que estem ordenant) és més petit que el pivot...
-		{
-			val[i - 1] = vtmp;  // Movem el valor temporal a la posició que acabem de deixar lliure (a la primera iteració al lloc on estava el pivot)
-			i++;  // passem al següent element 
-			vtmp = val[i];  // Ara que hem colocat el vtmp carreguem el següent valor a ordenar
-		}
-		else  // si es mes gran
-		{
-			vfi = val[f];  // guardem un valor a la dreta del pivot per a fer lloc per al nou valor
-			val[f] = vtmp;  // guardem el valor que estavem ordenant
-			f--;  // passem al següent element
-			vtmp = vfi;  // emmagatzemmem el valor temporal com a nou valor a ordenar
-		}
+  while (i <= f)
+   {
+    if (vtmp < pivot) {
+	    val[i-1] = vtmp;
+	    i ++;
+	    vtmp = val[i];
 	}
-	val[i - 1] = pivot;  // Un cop els dos índexos es creuen (i > f, i - 1 = f) coloquem el pivot a la posició i - 1 per a que els dos 
-	// sub vectors es trobin ordenats
+    else {
+            vfi = val[f];
+	    val[f] = vtmp;
+	    f --;
+	    vtmp = vfi;
+	}
+   }
+  val[i-1] = pivot;
  
- 	// En principi aquests dos ifs s'activen i porten dues trucades recursives que reordenen els dos subvectors
- 	// Si no es aixi, vol dir que el pivot escollit era major o menor que la resta d'elements en l'array (es el pitjor cas al escollir un pivot)
-	// o bé que el vector es molt curt 
- 	// Després repartim els dos subvectors en dues crides recursives, recalculant els indexos que passem a la funció
-	if (f > 1) qs(val, f);  // reordena el vector esquerra
-	if (i < ne - 1) qs(&val[i], ne - f - 1);  // reordena el vector dret
-}
+  if (f>1) qs(val,f);
+  if (i < ne-1) qs(&val[i],ne-f-1);
+ }
 
-// Funció merge
-// S'encarrega de mesclar dos vectors ordenats i contigus en memoria de la mateixa mida per a obtenir un vector output ordenat, 
-// situat en un altre espai de memoria.
+void merge2(int* val, int n,int *vo)
+ {
+  int vtmp;
+  int i,j,posi,posj; 
 
-void merge2(int* val, int n, int *vo)
-{
-	int i, posi, posj; 
-
-	posi = 0;  // posicio inicial en el vector esquerra
-	posj = (n / 2);  // posicio inicial en el vector dret //? redundant parenthesis
+   posi = 0;
+   posj = (n/2);
  
-	for (i = 0; i < n; i++)  // recorrem el conjunt dels dos vectors
-		// si hem acabat amb el vector j o no hem acabat el vector i & el valor al vector i es menor o igual que el valor al vector j 
-		if (((posi < n/2) && (val[posi] <= val[posj])) || (posj >= n)) 
-			vo[i] = val[posi++];
-		else 
-			vo[i] = val[posj++];
-}
+   for (i=0;i<n;i++)
+      if (((posi < n/2) && (val[posi] <= val[posj])) || (posj >= n))
+	  vo[i] = val[posi++];
+	else if (posj < n)
+	  vo[i] = val[posj++];
+ }
 
-int main(int nargs, char* args[])
+int main(int nargs,char* args[])
 {
-	int ndades, i, m, parts, porcio;
-	int *vin, *vout, *vtmp;
-	long long sum = 0;
+  int ndades,i,m,parts,porcio;
+  int *vin,*vout,*vtmp;
+  long long sum=0;
 
-	assert(nargs == 3);
+  assert(nargs == 3);
   
-	ndades = atoi(args[1]);
-	assert(ndades <= NN);
+  ndades = atoi(args[1]);
+  assert(ndades <= NN);
   
-	parts = atoi(args[2]);
-	if (parts < 2) 
-		assert("Han d'haver dues parts com a minim" == 0);
+  parts = atoi(args[2]);
+  if (parts < 2) assert("Han d'haver dues parts com a minim" == 0);
+  if (ndades % parts) assert("N ha de ser divisible per parts" == 0);
 
-	if (ndades % parts) 
-		assert("N ha de ser divisible per parts" == 0);
+  for(i=0;i<ndades;i++) valors[i]=rand()%MAX_INT;
 
-	for (i = 0; i < ndades; i++) 
-		valors[i] = rand() % MAX_INT;
+porcio = ndades/parts;
 
-	porcio = ndades / parts;
+// Quicksort a parts
+  for (i=0;i<parts;i++)
+    qs(&valors[i*porcio],porcio);
 
-	// Quicksort a parts
-	#pragma omp parallel for
-	for (i = 0; i < parts; i++)
-		qs(&valors[i * porcio], porcio);
+// Merge en arbre
+  vin = valors;
+  vout = valors2;
+  for (m = 2*porcio; m <= ndades; m *= 2)
+  {
+    for (i = 0; i < ndades; i += m)
+    	merge2(&vin[i],m,&vout[i]);
+    vtmp=vin;
+    vin=vout;
+    vout=vtmp;
+  }
 
-	// Merge en arbre
-	vin = valors;
-	vout = valors2;
-	for (m = 2 * porcio; m <= ndades; m *= 2)
-	{
-		#pragma omp parallel for
-		for (i = 0; i < ndades; i += m)
-			merge2(&vin[i], m, &vout[i]);
-		vtmp = vin;
-		vin = vout;
-    		vout = vtmp;
-  	}
-
-	// Validacio
-	for (i = 1; i < ndades; i++) 
-		assert(vin[i - 1] <= vin[i]);
-
-	for (i = 0; i < ndades; i += 100)
-		sum += vin[i];
-
-	printf("validacio %lld \n",sum);
-	exit(0);
+// Validacio
+  for (i=1;i<ndades;i++) assert(vin[i-1]<=vin[i]);
+  for (i=0;i<ndades;i+=100)
+    sum += vin[i];
+  printf("validacio %lld \n",sum);
+  exit(0);
 }
 
