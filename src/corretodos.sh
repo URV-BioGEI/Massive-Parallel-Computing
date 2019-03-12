768d5d5cabe57c10643b5221c0ee41c732c5ab16
AleixMT <aleixaretra@gmail.com>
2019-03-12 16:16:32 +0100

    results and run script

--- src/corretodos.sh
+++ src/corretodos.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
+rm results.txt
 binari=('QS_Paralel' 'Validation_For' 'Validation_ForNoWait' 'Validation_Sections');
-for name in ${array[@]}; do
-	echo "${binari[i]}"
+for name in ${binari[@]}; do
+	make $name
+	echo $name >> results.txt
 	i="0"
 	while [ $i -lt 10 ]
 	do
-		time=$(srun -p gat time ./${binari[i]} 300000000 32 2>&1 | grep "elapsed" | cut -d ' ' -f3 | cut -c 3-7)
+		time=$(srun -p gat time ./$name 300000000 32 2>&1 | grep "elapsed" | cut -d ' ' -f3 | cut -c 3-7)
 		echo "$time"
 		echo "$time " >> results.txt	
 		i=$[$i+1]
