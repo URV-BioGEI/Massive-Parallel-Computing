fe2ece000383848e87e0767188634a1dd81202e2
AleixMT <aleixaretra@gmail.com>
2019-03-12 21:18:01 +0100

    results

--- src/corretodos.sh
+++ src/corretodos.sh
@@ -2,21 +2,23 @@
 #!/bin/bash
 rm results.txt
 cores=('2' '4' '8' '16' '32')
-binari=('QS_Paralel' 'QS_ParalelNested' 'Validation_For' 'Validation_ForNoWait' 'Validation_Sections');
+binari=('QS_Paralel' 'QS_ParalelNoWait' 'Validation_For');
 for name in ${binari[@]}; do
 
 	echo "$name" >> results.txt
 	echo "$name"
-	i="0"
 	for core in ${cores[@]}; do
-		while [ $i -lt 10 ]
+		echo "$core" >> results.txt
+		i="0"
+		while [ $i -lt 5 ]
 		do
+			echo "$i"
 			echo "srun -p gat time ./$name 300000000 $core 2>&1"
 			times=$(srun -p gat time ./$name 300000000 $core 2>&1)
-			result=$(echo "$time" | grep "elapsed" | cut -d ' ' -f3 | cut -c 3-7)
+			result=$(echo "$times" | grep "elapsed" | cut -d ' ' -f3 | cut -c 3-7)
 			echo "result is $result"
 			echo "$times"
-			echo "$times " >> results.txt	
+			echo "$result" >> results.txt	
 			i=$[$i+1]
 		done
 	done
