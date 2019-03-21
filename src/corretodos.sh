f9b40725054ed7f2d3d94192114d1efee5124e69
AleixMT <aleixaretra@gmail.com>
2019-03-21 14:22:09 +0100

    modified script corretodos.sh to obtain result in CSV interpretable format

--- src/corretodos.sh
+++ src/corretodos.sh
@@ -8,13 +8,15 @@ samples='10'
 rm results.txt
 for binary in ${binaries[@]}; do
 	echo "$binary" | tee -a results.txt
-	for core in ${cores[@]}; do
-		echo "$core" | tee -a results.txt
-		for ((i=0;i<samples;i++)); do
-			times=$(srun -p gat time ./$binary $core 4 2>&1 )
-			echo "$times" | tee -a results.txt	
+	for server in ${servers[@]}; do
+		echo "$server" | tee -a results.txt
+		for core in ${cores[@]}; do
+			echo "$core" | tee -a results.txt
+			for ((i=0;i<samples;i++)); do
+				times=$(srun -p $server time ./$binary $core 2>&1 | grep "elapsed" | cut -d ' ' -f3 | cut -c 3-7)
+				echo "$times" | tee -a results.txt	
+			done
 		done
 	done
 done
-
 
