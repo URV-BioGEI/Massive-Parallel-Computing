0407cb3b568df014f30b0b60eb2eac5aad732d2e
AleixMT <aleixaretra@gmail.com>
2019-03-26 21:28:31 +0100

    modified makefile, hardcoded num_threads

--- src/corretodos.sh
+++ src/corretodos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cores=('4' '8' '16' '32')
-binaries=('P2.1_CPM_MarineRuiz');
+cores=('smoketest')
+binaries=('P1.2_CPM_MarineRuiz');
 servers=("gat" "roquer" "teen")
 samples='10'
 
@@ -13,7 +13,7 @@ for binary in ${binaries[@]}; do
 		for core in ${cores[@]}; do
 			echo "$core" | tee -a results.txt
 			for ((i=0;i<samples;i++)); do
-				times=$(srun -p $server time ./$binary $core 2>&1 | grep "elapsed" | cut -d ' ' -f3 | cut -c 3-7)
+				times=$(srun -p $server time ./$binary 2>&1 | grep "elapsed" | cut -d ' ' -f3 | cut -c 3-7)
 				echo "$times" | tee -a results.txt	
 			done
 		done
