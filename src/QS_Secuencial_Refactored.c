b62eb4f352a4b64f3295cf93c67c6c993eaa9348
AleixMT <aleixaretra@gmail.com>
2019-05-04 06:41:49 +0200

    version con declaracion de variables optimizadas i prints eliminados

--- src/QS_Secuencial_Refactored.c
+++ src/QS_Secuencial_Refactored.c
@@ -53,23 +53,6 @@ void qs(int *val, int ne)
 	if (i < ne - 1) qs(&val[i], ne - f - 1);
 }
 
-// Funció merge
-// 
-void merge2(int* val, int n, int *vo)
-{
-	int i, posi, posj; 
-
-	posi = 0;  // posicio inicial en el vector esquerra
-	posj = n / 2;  // posicio inicial en el vector dret //? redundant parenthesis
- 
-	for (i = 0; i < n; i++)  // recorrem el conjunt dels dos vectors
-		// si hem acabat amb el vector j o no hem acabat el vector i & el valor al vector i es menor o igual que el valor al vector j 
-		if (((posi < n/2) && (val[posi] <= val[posj])) || (posj >= n)) 
-			vo[i] = val[posi++];
-		else 
-			vo[i] = val[posj++];
-}
-
 // Funció merge reescrita
 // 
 int merge2_different(int* in1, int n_in1, int* in2, int n_in2, int *vo)
@@ -87,44 +70,19 @@ int merge2_different(int* in1, int n_in1, int* in2, int n_in2, int *vo)
 
 int main(int nargs,char* args[])
 {
-	int ndades, i, j, nivell, parts = 2, porcio = NN / parts, residu = NN % parts, acumulador = 0;
+
+	int closest_powerof2 = 0, ideal_num_processos, total_processos, id, parts = 7;
+	for (ideal_num_processos = 1; ideal_num_processos <= parts; ideal_num_processos *= 2) closest_powerof2++; 
+	
+	MPI_Init(&nargs, &args);	// Inicialitzem entorn paralel
+	MPI_Comm_rank(MPI_COMM_WORLD, &id); 	// Obtenim nombre de processos 
+	MPI_Comm_size(MPI_COMM_WORLD, &total_processos); 	// Obtenim el numero total de processos 
+
+	int num_received_values, i, counter = 0, proces_objectiu, porcio = NN / parts, residu = NN % parts, acumulador = 0;
 	int *vin, *vout, *vtmp, *vin2;
 	long long sum = 0;
-
-	int closest_powerof2 = 0;
-	int ideal_num_processos;
-
-	for (ideal_num_processos = 1; ideal_num_processos <= parts; ideal_num_processos *= 2) closest_powerof2++;
-	//printf("\nnum processos ideals: %i\nnum nivells de merge: %i", ideal_num_processos, closest_powerof2);
-	/* rank del proces    */
-	int id;
-	/* numero de processos        */
-	int total_processos;
-	/* rank de l'emissor */
-	int proces_font;
-	/* rank del receptor */
-	int proces_desti;
-	/* etiqueta dels missatges */
-	int tag = 0;
-	/* espai per al missatge      */
-	char msg[100];    
-	/* estat de la recepcio       */
 	MPI_Status estat;
-	/* estat de la request*/
 	MPI_Request request;
-  
-  	/* Inicialitzar MPI 
-	· argc Pointer to the number of arguments 
-	· argv Pointer to the argument vector 
-	This routine must be called by one thread only. 
-	That thread is called the main thread and must be the thread that calls MPI_Finalize. */
-	MPI_Init(&nargs, &args);
-    /* Obtenir el rank del proces  */
-	MPI_Comm_rank(MPI_COMM_WORLD, &id);
-	/* Obtenir el numero total de processos */
-	MPI_Comm_size(MPI_COMM_WORLD, &total_processos);
-  	
-	//parts = atoi(args[1]);
 	int *num_elements = malloc(sizeof(int) * parts);
 	int *offsets = malloc(sizeof(int) * parts);
 
@@ -139,11 +97,12 @@ int main(int nargs,char* args[])
         offsets[i] = acumulador;
         acumulador += num_elements[i];
 	}
+
+	// Vector initialization
 	if (id == 0)
 	{
 		for (i = 0; i < NN; i++) 
 			valors2[i] = rand();
-		printf("\n%d rand inicializtion done", id);
 	}
 	/* Enviar porcions
 	sendbuf: address of send buffer (choice, significant only at root) 
@@ -157,26 +116,19 @@ int main(int nargs,char* args[])
 	comm: communicator (handle) 
 	*/
 	MPI_Scatterv(&valors2, num_elements, offsets, MPI_INT, &valors, num_elements[id], MPI_INT, 0, MPI_COMM_WORLD);
-	printf("\nProces %d ha fet scatter", id);
 	qs(&valors[0], num_elements[id]);
-	printf("\nProces %d ha fet qs", id);
 	
 	vin = valors;
 	vin2 = valors2;
 	vout = valors3;
 
-	int num_received_values;
-	int counter = 0;
 	for (i = 2; i < ideal_num_processos * 2; i *= 2)
 	{
 		if (id % i > 0)  // envies
 		{
-			proces_font = id;
-			proces_desti = id - id % i;
-			printf("\nProces %d en iteracio %d envia %d dades a %d", id, counter, num_elements[id], proces_desti);
-			MPI_Isend (vin, num_elements[id], MPI_INT, proces_desti, 0, MPI_COMM_WORLD, &request);
+			proces_objectiu = id - id % i;
+			MPI_Isend (vin, num_elements[id], MPI_INT, proces_objectiu, 0, MPI_COMM_WORLD, &request);
 			MPI_Request_free(&request);
-			printf("\nProces %d en iteracio %d ha acabat la transmissió i espera a la barrera", id, counter);
 			MPI_Barrier(MPI_COMM_WORLD);
 			MPI_Finalize();
 
@@ -184,96 +136,41 @@ int main(int nargs,char* args[])
     		free(offsets);
 
     		exit(0);
-			//return 0;
 			// fin de hebra
 		}
 		else  // reps
 		{
-			proces_font = id + i / 2;
-			proces_desti = id;
-			if (proces_font != id && proces_font < parts)
+			proces_objectiu = id + i / 2;
+			if (proces_objectiu != id && proces_objectiu < parts)
 			{
-				printf("\nProces %d en iteracio %d INTENTA rebre dades de %d", id, counter, proces_font);
-				MPI_Recv(vin2, NN, MPI_INT, proces_font, 0, MPI_COMM_WORLD, &estat);
+				MPI_Recv(vin2, NN, MPI_INT, proces_objectiu, 0, MPI_COMM_WORLD, &estat);
 				MPI_Get_count(&estat, MPI_INT, &num_received_values);
-				
-				printf ("\nProces %i llegeix %i numeros i te %i a iteracio %i", id, num_received_values, num_elements[id], counter);
-				if (0)
-				{
-					printf("\nPrevia a Iteracio %i:\nvin2 ", counter);
-					for (j = 0; j < num_received_values; j++)
-						printf("%i ", vin2[j]);
-					printf("\nvin1 ");
-					for (j = 0; j < num_elements[id]; j++)
-						printf("%i ", vin[j]);
-				}
 				num_elements[id] = merge2_different(vin, num_elements[id], vin2, num_received_values, vout);
-				if (0)
-				{
-					printf("\n After a Iteracio %i: ", counter);
-					for (j = 0; j < num_elements[id]; j++)
-						printf("%i ", vout[j]);
-				}
-				printf ("\nProces %i a iteracio % i retorna %i", id, counter, num_elements[id]);
-				if (id == 0) printf("\ncounter %i i: %i ", counter, i);
-				
+
 				vtmp = vin; // copia temporal
 				vin = vout; // vin apunta a la sortida del merge
 				vout = vtmp; // vout ara apunta a les dades antigues
 				// vin2 s'anira matxacant pero no importa
-	
 			}
 		}
 		counter++;
-		if (id == 0) printf("\ncounter %i i: %i ", counter, i);
   	}
 
-	//if (id == 5) for (i = 0; i < num_elements[id]; i++) printf("%d ", valors2[i]);
-  	printf("\ndw");
   	MPI_Barrier(MPI_COMM_WORLD);
-  	printf("\ndw2");
     MPI_Finalize();
 
 	// Validacio
 	bool correct = 1;
 	for (i = 1; i < NN; i++) 
-	{
 		correct &= vin[i - 1] <= vin[i];
-	}
-	if (!correct) printf("\n Has sido un mal programador?? :P");
 
 	for (i = 0; i < NN; i += 100)
 		sum += vin[i];
 
-	printf("\nvalidacio %lld \n",sum);
+	printf("Validacio %lld\n",sum);
 
     free(num_elements);
     free(offsets);
 
 	return 0;
 }
-/*
-
-	// Merge en arbre
-	vin = valors;
-	vout = valors2;
-	for (m = 2 * porcio; m <= ndades; m *= 2)
-	{
-		for (i = 0; i < ndades; i += m)
-			merge2(&vin[i], m, &vout[i]);
-		vtmp = vin;
-		vin = vout;
-    	vout = vtmp;
-  	}
-
-	// Validacio
-	for (i = 1; i < ndades; i++) 
-		assert(vin[i - 1] <= vin[i]);
-
-	for (i = 0; i < ndades; i += 100)
-		sum += vin[i];
-
-	printf("validacio %lld \n",sum);
-	exit(0);
-}
-*/
